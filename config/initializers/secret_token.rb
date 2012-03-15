f96f84938884aeff44d76d109a6a1f32751b13bc
Ian Dorko <idorko@gmail.com>
2012-03-14 18:48:06 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IrcSeedBank::Application.config.secret_token = 'f53b99649f0d99a2c648951c1a6a35c5d45e4830d5c82b80aa36cf71434883b0d7fab158afaf82faf63a8e56d5ee0417a36569db18d3f4fd44f4bc11ced1886e'
