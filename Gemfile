564691f3bd766fb31dc41583530ad592c8b068de
Ian Dorko <idorko@gmail.com>
2012-03-14 21:11:21 -0600

    finished user login and stuff

--- Gemfile
+++ Gemfile
@@ -23,7 +23,7 @@ end
 gem 'jquery-rails'
 gem "rspec-rails", ">= 2.8.1", :group =>[:development, :test]
 gem "factory_girl_rails", ">= 1.7.0", :group => :test
-gem "email_spec", ">1.2.1", :group => :test
+gem "email_spec", ">= 1.2.1", :group => :test
 gem "cucumber-rails", ">= 1.3.0", :group => :test
 gem "capybara", ">= 1.1.2", :group => :test
 gem "database_cleaner", ">= 0.7.1", :group => :test
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,3 +1,4 @@
+
 IrcSeedBank::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
@@ -10,6 +11,9 @@ IrcSeedBank::Application.configure do
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
+
+	# ActionMailer Config
+	config.action_mailer.default_url_options = { :host => 'example.com' }
 
   # Log error messages when you accidentally call methods on nil
   config.whiny_nils = true
